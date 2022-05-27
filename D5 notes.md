3140b1178e17195276f36f0e78d9ebf7a1455310
Nathan <nathanmarquis23@gmail.com>
2022-05-27 08:26:54 -0600

    0527

---
+++ D5 notes.md
@@ -0,0 +1,6 @@
+
+// Output would be 9 to 0. Ends at 0 because 0 is a falsy statement when it gets there
+let i = 10
+while (i--){
+    console.log(i)
+}
--- Fun.js
+++ Fun.js
@@ -1,1 +1,13 @@
-console.log('hello world')  
+function strObjCounter(str) {
+    let sampObj = {}
+    for(let elem of str.toUpperCase()) {
+        if (elem in sampObj) {
+            sampObj[elem] ++
+        } else if (elem in sampObj === false) {
+            sampObj[elem] = 1
+        }
+    }
+return sampObj
+}
+
+console.log(strObjCounter('Greek'))
