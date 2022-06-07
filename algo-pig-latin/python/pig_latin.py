8dc32d0a2cbd858777f6b65e13c3bde56c478150
NathanMarquis <nathanmarquis23@gmail.com>
2022-06-07 07:26:23 -0600

    pig

--- algo-pig-latin/python/pig_latin.py
+++ algo-pig-latin/python/pig_latin.py
@@ -1,3 +1,28 @@
 def translate(word_or_phrase):
-  pass
+  word_list = word_or_phrase.split()
+  piggified_list = []
 
+  def piggy_word_converter(word):
+    vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+    consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z']
+
+    if word[0] in vowels:
+      pig_arr = list(word)
+      pig_arr.append('ay')
+      pig_word = ''.join(pig_arr)
+      return pig_word
+
+    elif word[0] in consonants:
+      pig_arr = list(word)
+      pig_arr.append(pig_arr.pop(0))
+      pig_arr.append('ay')
+      pig_word = ''.join(pig_arr)
+      return pig_word
+
+  for word in word_list:
+    piggified_list.append(piggy_word_converter(word))
+  
+    print (' '.join(piggified_list))
+    return ' '.join(piggified_list)
+
+# translate('you are cool guy')
